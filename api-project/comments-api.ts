f67d1a11d03b2b12707b767c8c9fd64496853ede
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2024-11-24 14:19:36 +0100

    Add files via upload

--- api-project/comments-api.ts
+++ api-project/comments-api.ts
@@ -1,2 +1,20 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
+import {IComment} from "./types";
+import {readFile} from "fs/promises";
 
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    if(req.url === '/api/comments' && req.method === 'GET'){
+        res.setHeader('Content-Type', 'application/json');
+        res.write('abc');
+        res.end();
+    }else{
+        res.statusCode = 400;
+        res.end('Not found :( ');
+    }
+});
+
+const PORT = 3000;
+const addressIP = '127.0.0.1';
+server.listen(PORT, addressIP, ()=>{
+    console.log(`Server runnibg on port ${PORT}`);
+});
