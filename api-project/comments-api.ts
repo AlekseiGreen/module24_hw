867ef5503a8366e205bb13bdb55729e76328eadf
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2024-11-24 20:17:29 +0100

    Add postman

--- api-project/comments-api.ts
+++ api-project/comments-api.ts
@@ -2,15 +2,35 @@ import { createServer, IncomingMessage, ServerResponse } from 'http';
 import {IComment} from "./types";
 import {readFile} from "fs/promises";
 
-const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-    if(req.url === '/api/comments' && req.method === 'GET'){
+
+const loadComments = async(): Promise<IComment[]> => {
+    const rawData = await readFile("mock-comments.json", "binary");
+    return JSON.parse(rawData.toString());
+}
+
+const server = createServer(async (req: IncomingMessage, res: ServerResponse) => {
+    if (req.url === '/api/comments' && req.method === 'GET') {
+        const comments = await loadComments();
+    
+    
         res.setHeader('Content-Type', 'application/json');
-        res.write('abc');
+        res.write(JSON.stringify(comments));
         res.end();
-    }else{
-        res.statusCode = 400;
-        res.end('Not found :( ');
-    }
+    } else if (req.url === '/api/comments' && req.method === 'POST') {
+        let rawBody = '';
+        req.on('data', (chunk) => {
+            rawBody += chunk.toString();
+        }); 
+        console.log("rb=", rawBody);
+    
+        req.on('end', () => {
+            console.log(JSON.parse(rawBody || null));
+            res.end("OK")
+        });
+    } else {
+        res.statusCode = 404;
+        res.end('Not found');
+    } 
 });
 
 const PORT = 3000;
