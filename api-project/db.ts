947e32fe88785b216523a4571ade5f571a7626e7
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2024-12-15 16:02:08 +0300

    Add files via upload

--- api-project/db.ts
+++ api-project/db.ts
@@ -1,20 +1,21 @@
-import mysql, {Connection} from "mysql2/promise";
+import mysql, { Connection } from "mysql2/promise";
 
-export async function initDataBase(): Promise<Connection|null>{
+export async function initDataBase(): Promise<Connection | null> {
     let connection: Connection | null = null;
-    try{
+  
+    try {
         connection = await mysql.createConnection({
             host: "localhost",
             port: 3306,
-            password: 'editor369',
+            password: '29aiVBpTG',
             user: "editor",
-            database: "ProductApplication",
+            database: "ProductsApplication",
         });
-    }
-    catch(e){
+    } catch (e) {
         console.error(e.message || e);
         return null;
     }
+  
     console.log(`Connection to DB ProductsApplication established`);
-    return connection; 
+    return connection;
 }
