ffff6b9e6bb5a51be6677236868d5b98a696145d
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2025-01-02 16:45:47 +0300

    Add files via upload

--- api-project/src/api/comments-api.ts
+++ api-project/src/api/comments-api.ts
@@ -6,6 +6,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { connection } from "../../index";
 import { mapCommentEntity } from '../services/mapping';
 import { OkPacket } from "mysql2";
+import { COMMENT_DUPLICATE_QUERY, INSERT_COMMENT_QUERY } from "../services/queries";
 
 
 const loadComments = async(): Promise<IComment[]> => {
@@ -101,36 +102,63 @@ commentsRouter.post('/', async(
 
 );
 
-commentsRouter.patch('/', async( req:Request < {}, {}, Partial<IComment> >, res:Response ) => {
-    const comments = await loadComments();
+commentsRouter.patch('/', async(
+  req:Request < {}, {}, Partial<IComment> >,
+  res:Response ) => {
+    try{
+      // block_1
+      let updateQuery ="UPDATE comments SET ";
+      const valuesToUpdate =[];
+      ["name", "body", "email"].forEach(fieldName=>{
+        if(req.body.hasOwnProperty(fieldName)) {
+          if(valuesToUpdate.length) {
+            updateQuery += ", ";
+          }
+          updateQuery += `${fieldName} = ?`;
+          valuesToUpdate.push(req.body[fieldName]);
+        }
+      });
+    
+      updateQuery += " WHERE comment_id = ?";
+      valuesToUpdate.push(req.body.id);
 
-    const targetCommentIndex = comments.findIndex(({ id }) => req.body.id === id);
-  
-    if (targetCommentIndex > -1) {
-      comments[targetCommentIndex] = { ...comments[targetCommentIndex], ...req.body }
-      await saveComments(comments);
-  
-      res.status(200);
-      res.send(comments[targetCommentIndex]);
-      return;
+      console.log("req.body.id=", req.body.id);
+
+      // Example-> updateQuery="UPDATE comments SET name = ?, email = ? WHERE comment_id = ?"
+      const [info] = await connection.query<OkPacket>(updateQuery, valuesToUpdate);
+      
+      if (info.affectedRows === 1) {
+        res.status(200);
+        res.end();
+        return;
+      }// END block_1
+    
+      // block_2
+      const newComment = req.body as CommentCreatePayload;
+      const validationResult = validateComment(newComment);
+    
+      if (validationResult) {
+        res.status(400);
+        res.send(validationResult);
+        return;
+      }// END block_2
+    
+      // block_3
+      const id = uuidv4();
+      await connection.query<OkPacket>(
+        INSERT_COMMENT_QUERY,
+        [id, newComment.email, newComment.name, newComment.body, newComment.productId]
+      );
+    
+      res.status(201);
+      res.send({... newComment,});
+      // END block_3
+    } catch(e) {
+      console.log(201);
+      res.status(500);
+      res.send("Server error");
     }
-  
-    const newComment = req.body as CommentCreatePayload;
-    const validationResult = validateComment(newComment);
-  
-    if (validationResult) {
-      res.status(400);
-      res.send(validationResult);
-      return;
-    }
-  
-    const id = uuidv4();
-    const commentToCreate = { ...newComment, id };
-    comments.push(commentToCreate);
-    await saveComments(comments);
-  
-    res.status(201);
-    res.send(commentToCreate);
+    
 });
 
 commentsRouter.delete('/:id', async( req:Request < { id:string } >, res:Response ) => {
